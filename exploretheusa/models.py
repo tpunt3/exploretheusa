33c8991a2d50068108e2ca7e45d4f9b2a952c7b7
Collin T Trowbridge <trowbrct@rose-hulman.edu>
2016-10-16 16:22:47 -0400

    add models.py file for Trip model

---
+++ exploretheusa/models.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import ndb
+
+class Trip(ndb.Model):
+    state = ndb.StringProperty()
+    city = ndb.StringProperty()
+    start_date = ndb.DateProperty()
+    end_date = ndb.DateProperty()
+    description = ndb.TextProperty()
