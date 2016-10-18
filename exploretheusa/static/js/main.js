8c5fe4a8651f48c290d14960de0f8a4dfc0ac451
Trent J Punt <punttj@rose-hulman.edu>
2016-10-17 21:30:17 -0400

    style and look updates

--- exploretheusa/static/js/main.js
+++ exploretheusa/static/js/main.js
@@ -1,5 +1,27 @@
+var rh = rh || {};
+rh.eu = rh.eu || {};
+
+rh.eu.enableButtons = function() {
+	
+	
+}
+
+rh.eu.mapUpdate = function(){
+//	think about removing and re-creating
+}
+
+
 $(document).ready(function() {
-    $('#map').usmap({
-    	stateStyles: {fill: 'white'}
-    });
-  });
+	$('#map').usmap({
+	  // The click action
+	  click: function(event, data) {
+	    console.log(data.name);
+	    rh.eu.mapUpdate();
+	  },
+	
+	stateSpecificStyles: {
+	    'MD': {fill: 'yellow'},
+	    'VA': {fill: 'teal'}
+	  }
+	});
+});
