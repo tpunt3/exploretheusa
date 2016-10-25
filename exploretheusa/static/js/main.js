891caa2dabb19a516cfd02b6740a5a1c5b438fc8
Trent J Punt <punttj@rose-hulman.edu>
2016-10-24 21:48:13 -0400

    expanding cards to view more

--- exploretheusa/static/js/main.js
+++ exploretheusa/static/js/main.js
@@ -5,6 +5,26 @@ rh.eu.enableButtons = function() {
   $("#attach-img-btn").click(function() {
 	rh.eu.triggerImageFileInput();
   });
+  
+  
+  $(".trip-card .header").click(function() {
+    var $header = $(this)
+    var $content = $header.next();
+    $content.slideToggle(250, function() {
+      if ($content.is(":visible")) {
+        $header.find(".edit-trip-btn").show();
+      } else {
+        $header.find(".edit-trip-btn").hide();
+      }
+    });
+  });
+  
+  
+  
+  $(".trip-card .edit-trip-btn").click(function(){
+	  console.log("woo");
+  });
+  
 }
 
 rh.eu.mainPageInit = function() {
