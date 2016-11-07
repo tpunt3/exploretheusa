2687cf6c82fc082395142a97af27f6bdfa7de31e
Collin T Trowbridge <trowbrct@rose-hulman.edu>
2016-11-06 19:43:22 -0500

    email on front end is fixed

--- exploretheusa/handlers/base_handlers.py
+++ exploretheusa/handlers/base_handlers.py
@@ -20,7 +20,7 @@ class BasePage(webapp2.RequestHandler):
     if user:
         email = user.email().lower()
         
-    values["user_email"] = email,
+    values["user_email"] = email
     values["logout_url"] = users.create_logout_url("/")
     values["form_action"] = blobstore.create_upload_url('/insert-trip')
     values["states"] = STATE_VALUES
